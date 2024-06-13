bc6212e3e5bc5adb5802b0d28363fb62fb90a6d9
Jagdeep Singh <24512034+jagdeepsb@users.noreply.github.com>
2024-06-13 16:23:31 -0400

    Update tutorials (#40)
    * Update tutorials
    
    * Update tutorials readme

--- tutorials/envs/simple_env.py
+++ tutorials/envs/simple_env.py
@@ -1,27 +1,34 @@
-from gym import spaces
+from gymnasium import spaces
 from evogym import EvoWorld
 from evogym.envs import EvoGymBase
 
+from typing import Optional, Dict, Any, Tuple
 import numpy as np
 import os
 
 class SimpleWalkerEnvClass(EvoGymBase):
 
-    def __init__(self, body, connections=None):
+    def __init__(
+        self,
+        body: np.ndarray,
+        connections: Optional[np.ndarray] = None,
+        render_mode: Optional[str] = None,
+        render_options: Optional[Dict[str, Any]] = None,
+    ):
 
         # make world
         self.world = EvoWorld.from_json(os.path.join('world_data', 'simple_walker_env.json'))
         self.world.add_from_array('robot', body, 1, 1, connections=connections)
 
         # init sim
-        EvoGymBase.__init__(self, self.world)
+        EvoGymBase.__init__(self, world=self.world, render_mode=render_mode, render_options=render_options)
 
         # set action space and observation space
         num_actuators = self.get_actuator_indices('robot').size
-        obs_size = self.reset().size
+        obs_size = self.reset()[0].size
 
-        self.action_space = spaces.Box(low= 0.6, high=1.6, shape=(num_actuators,), dtype=np.float)
-        self.observation_space = spaces.Box(low=-100.0, high=100.0, shape=(obs_size,), dtype=np.float)
+        self.action_space = spaces.Box(low= 0.6, high=1.6, shape=(num_actuators,), dtype=float)
+        self.observation_space = spaces.Box(low=-100.0, high=100.0, shape=(obs_size,), dtype=float)
 
         # set viewer to track objects
         self.default_viewer.track_objects('robot')
@@ -58,12 +65,12 @@ class SimpleWalkerEnvClass(EvoGymBase):
             self.get_relative_pos_obs("robot"),
             ))
 
-        # observation, reward, has simulation met termination conditions, debugging info
-        return obs, reward, done, {}
+        # observation, reward, has simulation met termination conditions, truncated, debugging info
+        return obs, reward, done, False, {}
 
-    def reset(self):
+    def reset(self, seed: Optional[int] = None, options: Optional[Dict[str, Any]] = None) -> Tuple[np.ndarray, Dict[str, Any]]:
         
-        super().reset()
+        super().reset(seed=seed, options=options)
 
         # observation
         obs = np.concatenate((
@@ -71,4 +78,4 @@ class SimpleWalkerEnvClass(EvoGymBase):
             self.get_relative_pos_obs("robot"),
             ))
 
-        return obs
+        return obs, {}
