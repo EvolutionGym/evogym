9a10d52a5270e1a0f0356759d79a4172c0538bfd
Jagdeep Bhatia <jagdeepsb150@gmail.com>
2024-06-12 21:45:17 -0400

    Update test

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,20 +1,19 @@
 from unittest import TestCase
 
 import evogym.envs
-import gym
+import gymnasium as gym
 from evogym import sample_robot
 
 
 class RenderTest(TestCase):
     def test_it(self):
         body, connections = sample_robot((5, 5))
-        env = gym.make("Walker-v0", body=body)
+        env = gym.make("Walker-v0", body=body, render_mode="human")
         env.reset()
 
         for _ in range(100):
             action = env.action_space.sample() - 1
             ob, reward, terminated, truncated, info = env.step(action)
-            env.render()
 
             if terminated or truncated:
                 env.reset()
